9b0456ca075496f155742e9149e1bca5d35fd72b
Neel Shah <neelmamania@gmail.com>
2021-01-14 16:55:48 +0200

    Update

---
+++ ipinfo_app/bin/ipinfo2.py
@@ -0,0 +1,147 @@
+import requests
+import json
+import sys
+import splunk.appserver.mrsparkle.lib.util as splunk_lib_util
+import re
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration
+import time
+import splunk.appserver.mrsparkle.lib.util as splunk_lib_util
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+
+
+maxbytes = 20000
+def get_logger(logger_id):
+	log_path = splunk_lib_util.make_splunkhome_path(["var", "log", "splunk","ipinfo"])
+	if not (os.path.isdir(log_path)):
+		os.makedirs(log_path)
+		
+	handler = RotatingFileHandler(log_path + '/ipinfo.log', maxBytes = maxbytes,backupCount = 20)
+	
+	formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+	handler.setFormatter(formatter)
+	logger = logging.getLogger(logger_id)
+	logger.setLevel(logging.ERROR)
+	logger.addHandler(handler)
+	return logger
+
+logger = get_logger("IPINFO")
+
+
+collection_name = "ipinfolookup"
+@Configuration(local=True)
+class ExtractDicom(StreamingCommand):
+    def stream(self, records):
+
+        ip_field = self.fieldnames[0]
+        collection = self.service.kvstore[collection_name]
+        for record in records:
+            ip_value = record.get(ip_field)
+            if ip_value != "":
+                response = collection.data.query(query = json.dumps({"_key":ip_value}))
+                logger.debug(response)
+                try:
+                    if(len(response)>0):
+                        response[0].pop('time')
+                        record.update(response[0])
+                    else:
+                        response = ipinfo(ip_value)
+                        if bool(response):    
+                            collection.data.insert(json.dumps(response))
+                            response.pop('time')
+                            record.update(response)
+                except Exception as e:
+                    logger.error(ip_value)
+                    logger.error(e)
+            yield record
+
+
+def ipinfo(ip_add):
+    
+    local_conf = splunk_lib_util.make_splunkhome_path(["etc","apps","ipinfo_app","local", "ip_info_setup.conf"])
+    default_conf = splunk_lib_util.make_splunkhome_path(["etc","apps","ipinfo_app","default", "ip_info_setup.conf"])
+    config = ConfigParser()
+    config.read([default_conf,local_conf])
+    url = config.get("ip_info_configuration","api_url")
+    token = config.get("ip_info_configuration","api_token")
+    enable = config.get("ip_info_configuration","proxy_enable")
+    proxy_url = config.get("ip_info_configuration","proxy_url")
+    response = ""
+    url = "https://ipinfo.io/"+ip_add
+    param = {"token" : token}    
+    try:
+        if enable == "No":
+            response = requests.request("GET", url, headers="", params=param)
+        else:
+            proxies = { 'https' : proxy_url}
+            response = requests.request("GET", url, headers="", params=param, proxies=proxies)
+    except Exception as e:
+        print(e)
+
+    json_data = json.loads(response.text)
+    result = {}
+    if response.status_code == 200:
+        if json_data.get("status") != 404:
+            if str(json_data.get("bogon")) is not "True":
+                result["ip"] = json_data["ip"]
+                result["city"] = json_data["city"] if 'city' in json_data else ""
+                result["region"] = json_data["region"] if 'region' in json_data else ""
+                result["country"] = json_data["country"] if 'country' in json_data else ""
+                result["loc"] = json_data["loc"] if 'loc' in json_data else ""
+                result["hostname"] = json_data["hostname"] if 'hostname' in json_data else ""
+                result["postal"] = json_data["postal"] if 'postal' in json_data else ""
+                result["org"] = json_data["org"] if 'org' in json_data else ""
+                result["subscription"] = "basic"
+                
+                if 'asn' in json_data:
+                    result["asn_asn"] = json_data["asn"]["asn"] if 'asn' in json_data["asn"] else ""
+                    result["asn_name"] = json_data["asn"]["name"] if 'name' in json_data["asn"] else ""
+                    result["asn_domain"] = json_data["asn"]["domain"] if 'domain' in json_data["asn"] else ""
+                    result["asn_route"] = json_data["asn"]["route"] if 'route' in json_data["asn"] else ""
+                    result["asn_type"] = json_data["asn"]["type"] if 'type' in json_data["asn"] else ""
+                    result["subscription"] = "standard"
+                else:
+                    result["asn_asn"] = ""
+                    result["asn_name"] = ""
+                    result["asn_domain"] = ""
+                    result["asn_route"] = ""
+                    result["asn_type"] = ""
+
+                if 'company' in json_data:
+                    result["company_name"] = json_data["company"]["name"] if 'name' in json_data["company"] else ""
+                    result["company_domain"] = json_data["company"]["domain"] if 'domain' in json_data["company"] else ""
+                    result["company_type"] = json_data["company"]["type"] if 'type' in json_data["company"] else ""
+                    result["subscription"] = "pro"
+                else:
+                    result["company_name"] = ""
+                    result["company_domain"] = ""
+                    result["company_type"] = ""
+                
+                if 'carrier' in json_data:
+                    result["carrier_name"] = json_data["carrier"]["name"] if 'name' in json_data["carrier"] else ""
+                    result["carrier_mcc"] = json_data["carrier"]["mcc"] if 'mcc' in json_data["carrier"] else ""
+                    result["carrier_mnc"] = json_data["carrier"]["mnc"] if 'mnc' in json_data["carrier"] else ""
+                else:
+                    result["carrier_name"] = ""
+                    result["carrier_mcc"] = ""
+                    result["carrier_mnc"] = ""
+                result["time"] = time.time()
+                result["_key"] = json_data["ip"]
+            else:
+                logger.info("Response From Ipinfo :" + response.text )
+        else:
+            logger.info("Response From Ipinfo :" + response.text )
+    else:
+        logger.error("Response Code : "+str(response.status_code) + "Response : " + response.text)
+    return result
+
+
+if __name__ == "__main__":
+    dispatch(ExtractDicom, sys.argv, sys.stdin, sys.stdout, __name__)
--- ipinfo_app/bin/ipinfo_updated.py
+++ ipinfo_app/bin/ipinfo_updated.py
@@ -17,13 +17,13 @@ except ImportError:
 
 
 
-maxbytes = 20000
+maxbytes = 200000
 def get_logger(logger_id):
 	log_path = splunk_lib_util.make_splunkhome_path(["var", "log", "splunk","ipinfo"])
 	if not (os.path.isdir(log_path)):
 		os.makedirs(log_path)
 		
-	handler = RotatingFileHandler(log_path + '/ipinfo.log', maxBytes = maxbytes,backupCount = 20)
+	handler = RotatingFileHandler(log_path + '/ipinfo.log', maxBytes = maxbytes,backupCount = 5)
 	
 	formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 	handler.setFormatter(formatter)
@@ -35,36 +35,49 @@ def get_logger(logger_id):
 logger = get_logger("IPINFO")
 
 
-collection_name = "ipinfolookup"
 @Configuration(local=True)
 class ExtractDicom(StreamingCommand):
     def stream(self, records):
-
+        record_list = {}
         ip_field = self.fieldnames[0]
-        collection = self.service.kvstore[collection_name]
+        count1=0
         for record in records:
             ip_value = record.get(ip_field)
             if ip_value != "":
-                response = collection.data.query(query = json.dumps({"_key":ip_value}))
-                logger.debug(response)
-                try:
-                    if(len(response)>0):
-                        response[0].pop('time')
-                        record.update(response[0])
-                    else:
-                        response = ipinfo(ip_value)
-                        if bool(response):    
-                            collection.data.insert(json.dumps(response))
-                            response.pop('time')
-                            record.update(response)
-                except Exception as e:
-                    logger.error(ip_value)
-                    logger.error(e)
-            yield record
+                record_list[ip_value] = record
+                count1 = count1 + 1
+                if count1%1000==0:
+                    list_of_ips=record_list.keys()
+                    request_ip = ', '.join(['"%s"' % w for w in list_of_ips])
+                    request_ip = '['+request_ip+']'
+                    #logger.error(request_ip)
+                    list_of_ip_details = getipinfo(request_ip)
+                    temp = record_list
+                    record_list = {}
+                    for key in temp.keys():
+                        try:
+                            record_v1 = temp[key]
+                            record_v1.update(list_of_ip_details[key])
 
+                        except Exception as e:
+                            logger.error(e)
+                        yield record_v1
+            else:
+                yield record
+                    
+                    
+        if count1%1000!=0:
+            list_of_ips=record_list.keys()
+            request_ip = ', '.join(['"%s"' % w for w in list_of_ips])
+            request_ip = '['+request_ip+']'
+            list_of_ip_details = getipinfo(request_ip)
+            for key in record_list.keys():
+                record_v1 = record_list[key]
+                record_v1.update(list_of_ip_details.get(key))
+                yield record_v1
 
-def ipinfo(ip_add):
-    
+ 
+def getipinfo(list_of_ips):
     local_conf = splunk_lib_util.make_splunkhome_path(["etc","apps","ipinfo_app","local", "ip_info_setup.conf"])
     default_conf = splunk_lib_util.make_splunkhome_path(["etc","apps","ipinfo_app","default", "ip_info_setup.conf"])
     config = ConfigParser()
@@ -74,74 +87,70 @@ def ipinfo(ip_add):
     enable = config.get("ip_info_configuration","proxy_enable")
     proxy_url = config.get("ip_info_configuration","proxy_url")
     response = ""
-    url = "https://ipinfo.io/"+ip_add
-    param = {"token" : token}    
+    url = "https://ipinfo.io/batch?token="+token
+    headers = {'Content-type': 'application/json'}
     try:
         if enable == "No":
-            response = requests.request("GET", url, headers="", params=param)
+            response = requests.request("POST", url, headers=headers, data=list_of_ips)
         else:
             proxies = { 'https' : proxy_url}
-            response = requests.request("GET", url, headers="", params=param, proxies=proxies)
+            response = requests.request("POST", url, headers=headers, data=list_of_ips, proxies=proxies)
     except Exception as e:
         print(e)
 
-    json_data = json.loads(response.text)
-    result = {}
+    response_json_data = json.loads(response.text)
+    response_result = {}
+    #logger.error(response_json_data)
     if response.status_code == 200:
-        if json_data.get("status") != 404:
-            if str(json_data.get("bogon")) is not "True":
-                result["ip"] = json_data["ip"]
-                result["city"] = json_data["city"] if 'city' in json_data else ""
-                result["region"] = json_data["region"] if 'region' in json_data else ""
-                result["country"] = json_data["country"] if 'country' in json_data else ""
-                result["loc"] = json_data["loc"] if 'loc' in json_data else ""
-                result["hostname"] = json_data["hostname"] if 'hostname' in json_data else ""
-                result["postal"] = json_data["postal"] if 'postal' in json_data else ""
-                result["org"] = json_data["org"] if 'org' in json_data else ""
-                result["subscription"] = "basic"
-                
-                if 'asn' in json_data:
-                    result["asn_asn"] = json_data["asn"]["asn"] if 'asn' in json_data["asn"] else ""
-                    result["asn_name"] = json_data["asn"]["name"] if 'name' in json_data["asn"] else ""
-                    result["asn_domain"] = json_data["asn"]["domain"] if 'domain' in json_data["asn"] else ""
-                    result["asn_route"] = json_data["asn"]["route"] if 'route' in json_data["asn"] else ""
-                    result["asn_type"] = json_data["asn"]["type"] if 'type' in json_data["asn"] else ""
-                    result["subscription"] = "standard"
-                else:
-                    result["asn_asn"] = ""
-                    result["asn_name"] = ""
-                    result["asn_domain"] = ""
-                    result["asn_route"] = ""
-                    result["asn_type"] = ""
+        for key in response_json_data.keys():
+            result = {}
+            json_data = response_json_data[key]
+            if json_data.get("status") != 404:
+                if str(json_data.get("bogon")) is not "True":
+                    result["ip"] = json_data["ip"]
+                    result["city"] = json_data["city"] if 'city' in json_data else ""
+                    result["region"] = json_data["region"] if 'region' in json_data else ""
+                    result["country"] = json_data["country"] if 'country' in json_data else ""
+                    result["loc"] = json_data["loc"] if 'loc' in json_data else ""
+                    result["hostname"] = json_data["hostname"] if 'hostname' in json_data else ""
+                    result["postal"] = json_data["postal"] if 'postal' in json_data else ""
+                    result["org"] = json_data["org"] if 'org' in json_data else ""
+                    result["subscription"] = "basic"
+                    
+                    if 'asn' in json_data:
+                        result["asn_asn"] = json_data["asn"]["asn"] if 'asn' in json_data["asn"] else ""
+                        result["asn_name"] = json_data["asn"]["name"] if 'name' in json_data["asn"] else ""
+                        result["asn_domain"] = json_data["asn"]["domain"] if 'domain' in json_data["asn"] else ""
+                        result["asn_route"] = json_data["asn"]["route"] if 'route' in json_data["asn"] else ""
+                        result["asn_type"] = json_data["asn"]["type"] if 'type' in json_data["asn"] else ""
+                        result["subscription"] = "standard"
+                    else:
+                        result["asn_asn"] = ""
+                        result["asn_name"] = ""
+                        result["asn_domain"] = ""
+                        result["asn_route"] = ""
+                        result["asn_type"] = ""
 
-                if 'company' in json_data:
-                    result["company_name"] = json_data["company"]["name"] if 'name' in json_data["company"] else ""
-                    result["company_domain"] = json_data["company"]["domain"] if 'domain' in json_data["company"] else ""
-                    result["company_type"] = json_data["company"]["type"] if 'type' in json_data["company"] else ""
-                    result["subscription"] = "pro"
-                else:
-                    result["company_name"] = ""
-                    result["company_domain"] = ""
-                    result["company_type"] = ""
-                
-                if 'carrier' in json_data:
-                    result["carrier_name"] = json_data["carrier"]["name"] if 'name' in json_data["carrier"] else ""
-                    result["carrier_mcc"] = json_data["carrier"]["mcc"] if 'mcc' in json_data["carrier"] else ""
-                    result["carrier_mnc"] = json_data["carrier"]["mnc"] if 'mnc' in json_data["carrier"] else ""
-                else:
-                    result["carrier_name"] = ""
-                    result["carrier_mcc"] = ""
-                    result["carrier_mnc"] = ""
-                result["time"] = time.time()
-                result["_key"] = json_data["ip"]
-            else:
-                logger.info("Response From Ipinfo :" + response.text )
-        else:
-            logger.info("Response From Ipinfo :" + response.text )
-    else:
-        logger.error("Response Code : "+str(response.status_code) + "Response : " + response.text)
-    return result
-
+                    if 'company' in json_data:
+                        result["company_name"] = json_data["company"]["name"] if 'name' in json_data["company"] else ""
+                        result["company_domain"] = json_data["company"]["domain"] if 'domain' in json_data["company"] else ""
+                        result["company_type"] = json_data["company"]["type"] if 'type' in json_data["company"] else ""
+                        result["subscription"] = "pro"
+                    else:
+                        result["company_name"] = ""
+                        result["company_domain"] = ""
+                        result["company_type"] = ""
+                    
+                    if 'carrier' in json_data:
+                        result["carrier_name"] = json_data["carrier"]["name"] if 'name' in json_data["carrier"] else ""
+                        result["carrier_mcc"] = json_data["carrier"]["mcc"] if 'mcc' in json_data["carrier"] else ""
+                        result["carrier_mnc"] = json_data["carrier"]["mnc"] if 'mnc' in json_data["carrier"] else ""
+                    else:
+                        result["carrier_name"] = ""
+                        result["carrier_mcc"] = ""
+                        result["carrier_mnc"] = ""
+            response_result[key] = result
+    return response_result
 
 if __name__ == "__main__":
     dispatch(ExtractDicom, sys.argv, sys.stdin, sys.stdout, __name__)
